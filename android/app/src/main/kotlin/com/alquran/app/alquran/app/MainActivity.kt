7fc8b6569fa110ee3f93051b6cb0618739bf1454
fathir <2210631170090@student.unsik.ac.id>
2025-11-17 14:43:36 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/alquran/app/alquran/app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.alquran.app.alquran_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main_development.dart
@@ -0,0 +1,29 @@
+import 'dart:ui';
+
+import 'package:alquran_app/app/app.dart';
+import 'package:alquran_app/bootstrap.dart';
+import 'package:alquran_app/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter/material.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  FlutterError.onError = (errorDetails) {
+    FirebaseCrashlytics.instance.recordFlutterFatalError(errorDetails);
+  };
+
+  await FirebaseCrashlytics.instance.setCrashlyticsCollectionEnabled(true);
+
+  // Pass all uncaught asynchronous errors that aren't handled by the Flutter framework to Crashlytics
+  PlatformDispatcher.instance.onError = (error, stack) {
+    FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+    return true;
+  };
+  await bootstrap(() => const App());
+}
